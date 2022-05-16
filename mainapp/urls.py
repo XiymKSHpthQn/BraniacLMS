81cf1592dd92365aa6575b1450b505dc98cb4f9a
Andrey <kad.gb@mail.ru>
2022-05-16 21:03:20 +0200

    lesson_2 Static site with Django

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,6 +6,10 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", views.HelloWorldView.as_view()),
-    path("<str:word>/", views.check_kwargs),
+    path("", views.MainPageView.as_view()),
+    path("news/", views.NewsPageView.as_view()),
+    path("courses/", views.CoursesPageView.as_view()),
+    path("contacts/", views.ContactsPageView.as_view()),
+    path("doc_site/", views.DocSitePageView.as_view()),
+    path("login/", views.LoginPageView.as_view()),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,25 @@
-from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import TemplateView
 
 
-class HelloWorldView(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse("Hello world")
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
 
 
-def check_kwargs(request, **kwargs):
-    return HttpResponse(f"kwargs:<br>{kwargs}")
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
