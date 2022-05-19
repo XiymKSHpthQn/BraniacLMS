b2e3ca868cc52a8043f3eabdef63090df1c276d3
Andrey <kad.gb@mail.ru>
2022-05-19 21:17:19 +0200

    lesson_3 Urls in templates

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", RedirectView.as_view(url="mainapp/")),
-    path("mainapp/", include("mainapp.urls")),
+    path("mainapp/", include("mainapp.urls", namespace="mainapp")),
 ]
