4c4a34b16890ca037767d6bcf1ffc64f844e2582
Andrey <kad.gb@mail.ru>
2022-05-19 19:41:09 +0200

    Lesson 2 (#1)
    lesson_2 Lesson 2 ready

---
+++ config/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="mainapp/")),
+    path("mainapp/", include("mainapp.urls")),
+]
---
+++ mainapp/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import TemplateView
+
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        return context
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
