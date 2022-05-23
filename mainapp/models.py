48c7b889807c17efdb262edbc42325bcd96d047d
Andrey <kad.gb@mail.ru>
2022-05-23 19:59:03 +0200

    lesson_4 Models

--- mainapp/models.py
+++ mainapp/models.py
@@ -55,3 +55,18 @@ class Lesson(models.Model):
 
     class Meta:
         ordering = ("course", "num")
+
+
+class CourseTeachers(models.Model):
+    course = models.ManyToManyField(Courses)
+    name_first = models.CharField(max_length=128, verbose_name="Name")
+    name_second = models.CharField(max_length=128, verbose_name="Surname")
+    day_birth = models.DateField(verbose_name="Birth date")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return "{0:0>3} {1} {2}".format(self.pk, self.name_second, self.name_first)
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
