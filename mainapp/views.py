6684168f5b7f572a0cc67606c195a00e3befa522
Andrey <kad.gb@mail.ru>
2022-05-23 19:03:30 +0200

    Lesson 3 (#2)
    lesson_3 Lesson ready

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
 
@@ -14,6 +16,15 @@ class NewsPageView(TemplateView):
         # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
         context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
+        return context
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
         return context
 
 
