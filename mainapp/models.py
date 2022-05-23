87abfcaad77c484563e6684884601766f37a3a1b
Andrey <kad.gb@mail.ru>
2022-05-23 19:45:42 +0200

    lesson_4 News model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=256, verbose_name="Title")
+    preambule = models.CharField(max_length=1024, verbose_name="Preambule")
+    body = models.TextField(blank=True, null=True, verbose_name="Body")
+    body_as_markdown = models.BooleanField(default=False, verbose_name="As markdown")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.title}"
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
