648ba2fc02b13693222ac92218bbcd02f3583325
Andrey <kad.gb@mail.ru>
2022-05-23 21:30:10 +0200

    lesson_4 Display model's data

--- mainapp/models.py
+++ mainapp/models.py
@@ -18,7 +18,14 @@ class News(models.Model):
         self.save()
 
 
+class CoursesManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
+
 class Courses(models.Model):
+    objects = CoursesManager()
+
     name = models.CharField(max_length=256, verbose_name="Name")
     description = models.TextField(verbose_name="Description", blank=True, null=True)
     description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
