8c0977e029d1439a4225a014a66685fce771ef0a
Andrey <kad.gb@mail.ru>
2022-05-26 19:09:29 +0200

    Lesson 4 (#3)
    lesson_4 Lesson 4 ready
    
    * lesson_4 Display model's data

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,1 +1,79 @@
-# Create your models here.
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=256, verbose_name="Title")
+    preambule = models.CharField(max_length=1024, verbose_name="Preambule")
+    body = models.TextField(blank=True, null=True, verbose_name="Body")
+    body_as_markdown = models.BooleanField(default=False, verbose_name="As markdown")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.title}"
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+
+class CoursesManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
+
+class Courses(models.Model):
+    objects = CoursesManager()
+
+    name = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
+    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.name}"
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    num = models.PositiveIntegerField(verbose_name="Lesson number")
+    title = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.course.name} | {self.num} | {self.title}"
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+    class Meta:
+        ordering = ("course", "num")
+
+
+class CourseTeachers(models.Model):
+    course = models.ManyToManyField(Courses)
+    name_first = models.CharField(max_length=128, verbose_name="Name")
+    name_second = models.CharField(max_length=128, verbose_name="Surname")
+    day_birth = models.DateField(verbose_name="Birth date")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return "{0:0>3} {1} {2}".format(self.pk, self.name_second, self.name_first)
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,8 +8,13 @@ app_name = MainappConfig.name
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
-    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
-    path("courses/", views.CoursesPageView.as_view(), name="courses"),
+    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
+    path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path(
+        "courses/<int:pk>/",
+        views.CoursesDetailView.as_view(),
+        name="courses_detail",
+    ),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
     path("login/", views.LoginPageView.as_view(), name="login"),
