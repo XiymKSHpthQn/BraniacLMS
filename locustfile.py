38247ec07548478431d0fb2a67d553d1d2835d35
Andrey <kad.gb@mail.ru>
2022-06-09 22:06:54 +0200

    lesson_9 Load testing

---
+++ locustfile.py
@@ -0,0 +1,15 @@
+from locust import HttpUser, task
+
+SERVER_IP_ADDR = "188.93.210.17"
+
+
+class LoadTestingBraniacLMS(HttpUser):
+    @task
+    def test_some_pages_open(self):
+        # Mainapp
+        self.client.get(f"http://{SERVER_IP_ADDR}/mainapp/")
+        self.client.get(f"http://{SERVER_IP_ADDR}/mainapp/courses/")
+        self.client.get(f"http://{SERVER_IP_ADDR}/mainapp/courses/1/")
+        # Authapp
+        self.client.get(f"http://{SERVER_IP_ADDR}/authapp/register/")
+        self.client.get(f"http://{SERVER_IP_ADDR}/authapp/login/")
