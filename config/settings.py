70d8e28cda01b586784f2970d7ded5ab0999c7d0
Andrey <kad.gb@mail.ru>
2022-08-22 20:48:44 +0200

    lesson_3 New context processor

--- config/settings.py
+++ config/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "mainapp.context_processors.example.simple_context_processor",
             ],
         },
     },
