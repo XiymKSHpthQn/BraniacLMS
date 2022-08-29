9d71bfe5c80acd93f12850ceaabf08d3f600c5bb
Andrey <kad.gb@mail.ru>
2022-08-29 20:43:43 +0200

    lesson_5 Authapp done

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", RedirectView.as_view(url="mainapp/")),
     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
+    path("authapp/", include("authapp.urls", namespace="authapp")),
 ]
 
 if settings.DEBUG:
