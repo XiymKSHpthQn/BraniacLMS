b2dfe194548b4c004179e644965eb3d6c315139c
Andrey <kad.gb@mail.ru>
2022-08-29 20:28:40 +0200

    lesson_5 Registration

--- authapp/views.py
+++ authapp/views.py
@@ -1,7 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
+from django.views.generic import TemplateView
+
+from authapp import models
 
 
 class CustomLoginView(LoginView):
@@ -29,3 +34,37 @@ class CustomLogoutView(LogoutView):
     def dispatch(self, request, *args, **kwargs):
         messages.add_message(self.request, messages.INFO, _("See you later!"))
         return super().dispatch(request, *args, **kwargs)
+
+
+class RegisterView(TemplateView):
+    template_name = "registration/register.html"
+
+    def post(self, request, *args, **kwargs):
+        try:
+            if all(
+                (
+                    request.POST.get("username"),
+                    request.POST.get("email"),
+                    request.POST.get("password1"),
+                    request.POST.get("password1") == request.POST.get("password2"),
+                )
+            ):
+                new_user = models.CustomUser.objects.create(
+                    username=request.POST.get("username"),
+                    first_name=request.POST.get("first_name"),
+                    last_name=request.POST.get("last_name"),
+                    age=request.POST.get("age") if request.POST.get("age") else 0,
+                    avatar=request.FILES.get("avatar"),
+                    email=request.POST.get("email"),
+                )
+                new_user.set_password(request.POST.get("password1"))
+                new_user.save()
+                messages.add_message(request, messages.INFO, _("Registration success!"))
+                return HttpResponseRedirect(reverse_lazy("authapp:login"))
+        except Exception as exp:
+            messages.add_message(
+                request,
+                messages.WARNING,
+                mark_safe(f"Something goes worng:<br>{exp}"),
+            )
+            return HttpResponseRedirect(reverse_lazy("authapp:register"))
