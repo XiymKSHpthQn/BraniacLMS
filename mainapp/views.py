5a8a40fc4c1d10c7e81b3a75d0b141778d41b880
Andrey <kad.gb@mail.ru>
2022-09-01 20:59:40 +0200

    lesson_6 News CRUD and permissions

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView
 
 from mainapp import models as mainapp_models
 
@@ -8,24 +10,36 @@ class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
 
-class NewsPageView(TemplateView):
-    template_name = "mainapp/news.html"
+class NewsListView(ListView):
+    model = mainapp_models.News
+    paginate_by = 5
 
-    def get_context_data(self, **kwargs):
-        # Get all previous data
-        context = super().get_context_data(**kwargs)
-        # Create your own data
-        context["news_qs"] = mainapp_models.News.objects.all()[:5]
-        return context
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
 
 
-class NewsPageDetailView(TemplateView):
-    template_name = "mainapp/news_detail.html"
+class NewsCreateView(PermissionRequiredMixin, CreateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.add_news",)
 
-    def get_context_data(self, pk=None, **kwargs):
-        context = super().get_context_data(pk=pk, **kwargs)
-        context["news_object"] = get_object_or_404(mainapp_models.News, pk=pk)
-        return context
+
+class NewsDetailView(DetailView):
+    model = mainapp_models.News
+
+
+class NewsUpdateView(PermissionRequiredMixin, UpdateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.change_news",)
+
+
+class NewsDeleteView(PermissionRequiredMixin, DeleteView):
+    model = mainapp_models.News
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.delete_news",)
 
 
 class CoursesListView(TemplateView):
