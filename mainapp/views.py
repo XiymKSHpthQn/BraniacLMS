771e89f7896229f58f440f4f65e298fbed5d9e32
Andrey <kad.gb@mail.ru>
2022-09-05 21:07:12 +0200

    lesson_7 Email via celery

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,20 @@
 import logging
 
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from django.core.cache import cache
 from django.http import FileResponse, JsonResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView, View
 
 from mainapp import forms as mainapp_forms
 from mainapp import models as mainapp_models
+from mainapp import tasks as mainapp_tasks
 
 logger = logging.getLogger(__name__)
 
@@ -103,6 +107,36 @@ class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
 
 class ContactsPageView(TemplateView):
     template_name = "mainapp/contacts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactsPageView, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["form"] = mainapp_forms.MailFeedbackForm(user=self.request.user)
+        return context
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            cache_lock_flag = cache.get(f"mail_feedback_lock_{self.request.user.pk}")
+            if not cache_lock_flag:
+                cache.set(
+                    f"mail_feedback_lock_{self.request.user.pk}",
+                    "lock",
+                    timeout=300,
+                )
+                messages.add_message(self.request, messages.INFO, _("Message sended"))
+                mainapp_tasks.send_feedback_mail.delay(
+                    {
+                        "user_id": self.request.POST.get("user_id"),
+                        "message": self.request.POST.get("message"),
+                    }
+                )
+            else:
+                messages.add_message(
+                    self.request,
+                    messages.WARNING,
+                    _("You can send only one message per 5 minutes"),
+                )
+        return HttpResponseRedirect(reverse_lazy("mainapp:contacts"))
 
 
 class DocSitePageView(TemplateView):
